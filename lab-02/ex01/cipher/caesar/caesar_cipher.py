0c3adac0203235b5f8dc354333fea3a8ac4a78c8
Sinkerr <112935022+Sinkerr@users.noreply.github.com>
2025-03-11 16:17:13 +0700

    Add files via upload

--- lab-02/ex01/cipher/caesar/caesar_cipher.py
+++ lab-02/ex01/cipher/caesar/caesar_cipher.py
@@ -3,13 +3,13 @@ from cipher.caesar import ALPHABET
 class CaesarCipher:
     def __init__(self):
         self.alphabet = ALPHABET
-
+        
     def encrypt_text(self, text: str, key: int) -> str:
         alphabet_len = len(self.alphabet)
         text = text.upper()
         encrypted_text = []
         for letter in text:
-            letter_index  = self.alphabet.index(letter)
+            letter_index = self.alphabet.index(letter)
             output_index = (letter_index + key) % alphabet_len
             output_letter = self.alphabet[output_index]
             encrypted_text.append(output_letter)
